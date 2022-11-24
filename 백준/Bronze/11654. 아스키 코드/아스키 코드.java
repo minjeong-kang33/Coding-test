641beb68b7e1cdf40c6dc849d36358ce682ca5b9
강 민정 <117153836+minjeong-kang33@users.noreply.github.com>
2022-11-24 13:45:05 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17584 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.next().charAt(0);
+		in.close();
+
+		System.out.println(n);
+
+	}
+
+}
