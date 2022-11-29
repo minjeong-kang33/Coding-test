885f720c070d6547fe45ba3f71cc729f03f1c32d
강 민정 <117153836+minjeong-kang33@users.noreply.github.com>
2022-11-29 13:54:09 +0900

    [Bronze III] Title: 알람 시계, Time: 232 ms, Memory: 18492 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -6,18 +6,18 @@ public class Main {
 		Scanner in = new Scanner(System.in);
 		int H = in.nextInt();
 		int M = in.nextInt();
-
 		in.close();
 
 		if (M < 45) {
 			H--;
-			M = 60 - (45 - M);
+			M += 15;
 			if (H < 0) {
 				H = 23;
 			}
 			System.out.println(H + " " + M);
 		} else {
-			System.out.println(H + " " + (M - 45));
+			M = M - 45;
+			System.out.println(H + " " + M);
 		}
 
 	}
