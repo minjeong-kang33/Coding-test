c497880701df3e843be5f296853653df3bf2b4e4
강 민정 <117153836+minjeong-kang33@users.noreply.github.com>
2022-11-30 11:17:43 +0900

    [Bronze II] Title: 손익분기점, Time: 208 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		int C = in.nextInt();
+		in.close();
+
+		if (C <= B) {
+			System.out.println("-1");
+		} else {
+			System.out.println((A / (C - B)) + 1);
+		}
+	}
+}
