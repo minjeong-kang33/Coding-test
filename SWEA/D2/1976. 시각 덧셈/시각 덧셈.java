265e137a0226a5b694132a3bb903dc602458d9fd
강 민정 <117153836+minjeong-kang33@users.noreply.github.com>
2023-01-26 09:09:31 +0900

    [D2] Title: 시각 덧셈, Time: 130 ms, Memory: 20,772 KB -BaekjoonHub

---
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.java
@@ -0,0 +1,65 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+
+		for (int t_case = 1; t_case <= T; t_case++) {
+			int hour1 = sc.nextInt();
+			int min1 = sc.nextInt();
+			int hour2 = sc.nextInt();
+			int min2 = sc.nextInt();
+
+			int new_h = hour1 + hour2;
+			int new_m = min1 + min2;
+
+			if (new_m >= 60) {
+				new_h += new_m / 60;
+				new_m -= (new_m / 60) * 60;
+			}
+			if (new_h > 12) {
+				new_h -= 12;
+			}
+
+			System.out.println("#" + t_case + " " + new_h + " " + new_m);
+		}
+		sc.close();
+
+	}
+
+}
