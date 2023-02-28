4501cb56470c2b3027107f774bfabdddd8459bba
강 민정 <117153836+minjeong-kang33@users.noreply.github.com>
2023-02-28 16:15:47 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -1,18 +1,18 @@
 class Solution {
     public int solution(int[] array) {
         int count = 0;
-        
-		for(int i=0; i<array.length; i++) {
-			String num = Integer.toString(array[i]);
-			String[] arr = num.split("");
-			
-			for(int j=0; j<arr.length; j++) {
-				if(arr[j].equals("7")) {
-					count++;
-				}
-			}
-		}
-		return count;
-	}
+
+        for(int i=0; i<array.length; i++) {
+           while(array[i] > 0){
+               //array[i]의 일의 자리수가 7인지 확인
+               if(array[i] % 10 == 7){
+                   count++;
+               }
+               //array[i]의 십의 자리수를 일의 자리수로 만든 후 다시 반복
+               array[i] = array[i]/10;
+           }
+        }
+        return count;
+    }
 
 }
