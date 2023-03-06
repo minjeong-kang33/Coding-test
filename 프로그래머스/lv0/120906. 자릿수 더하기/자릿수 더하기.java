a3e7d192b93d8a034a8b306da2659faf5feec8ff
강 민정 <117153836+minjeong-kang33@users.noreply.github.com>
2023-03-06 23:54:46 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.13 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        while(n/10>0) {
-			answer+=(n%10);
-			n/=10;
-		}
-		return(answer+n);
+    	int answer = 0;
+    	String[] strArr = Integer.toString(n).split("");
+    	
+    	for(int i=0; i<strArr.length;i++) {
+    		answer+= Integer.parseInt(strArr[i]);
+    	}
+    	 return answer;
     }
 }
