24d5b2794a19b8eedbfa99977c90aeaac45b7991
강 민정 <117153836+minjeong-kang33@users.noreply.github.com>
2023-06-25 10:22:10 +0900

    [Bronze IV] Title: 숫자의 합, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -4,14 +4,17 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		String str = sc.next();
-		sc.close();
 		
-		int answer = 0;
-		for(int i=0; i<N; i++) {
-			//charAt은 아스키코드로 변환하므로 -48/-'0'필요
-			answer += str.charAt(i)-'0';
+		// 받은 숫자는 String형으로 받은 후 하나씩 쪼개어 char[]에 담음
+		String sNum = sc.next();
+		char[] cNum = sNum.toCharArray();
+
+		int sum = 0;
+		for (int i = 0; i < cNum.length; i++) {
+			// cNum을 int형으로 바꾼 후 sum에 누적시킴
+			sum += cNum[i] - '0';
 		}
-		System.out.println(answer);
+
+		System.out.print(sum);
 	}
 }
