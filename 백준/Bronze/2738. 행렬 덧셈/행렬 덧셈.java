34fbc230cce85d9bfe4c1c8443293e816854497c
강 민정 <117153836+minjeong-kang33@users.noreply.github.com>
2023-09-18 21:02:57 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 408 ms, Memory: 19052 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,39 +1,36 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt(); //N행M열의 배열이 주어짐
-        
-        //배열은 0부터 시작하므로 N+1행, M+1행의 열을 갖는 2차원 배열생성
-        int[][] A = new int[N+1][M+1]; 
-        int[][] B = new int[N+1][M+1];
-        
-        //2차원배열 A에 받은 수를 담음
-        for(int i=1; i<=N; i++){
-            for(int j=1; j<=M; j++){
-                A[i][j] = sc.nextInt(); 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] A = new int[N][M];
+        int[][] B = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < M; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        //2차원배열 B에 받은 수를 담음
-        for(int i=1; i<=N; i++){
-            for(int j=1; j<=M; j++){
-               B[i][j] = sc.nextInt(); 
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < M; j++) {
+                B[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
-        int [][] answer = new int [N+1][M+1];
-        for(int i=1; i<=N; i++){
-            for(int j=1; j<=M; j++){
-                answer[i][j] = A[i][j] + B[i][j];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(A[i][j] + B[i][j] + " ");
             }
+            System.out.println();
         }
- 
-        for(int i=1; i<=N; i++){
-            for(int j=1; j<=M; j++){
-                System.out.print(answer[i][j] + " ");
-            }System.out.println();
-        }
+
+
     }
 }
